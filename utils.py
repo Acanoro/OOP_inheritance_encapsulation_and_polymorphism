24e552e044348c676901a9bb4773496fcb4b8edb
Acanoro <ntumasevic@gmail.com>
2024-02-01 11:58:04 +0300

    optimized the utils.py file

--- utils.py
+++ utils.py
@@ -1,20 +1,20 @@
-def average_homework_grade(list_student, name_course):
+def average_rating(list_student, name_course, attribute_name):
     all_values = []
 
     for student in list_student:
-        if name_course in student.grades:
-            for value in student.grades[name_course]:
+        if name_course in getattr(student, attribute_name):
+            for value in getattr(student, attribute_name)[name_course]:
                 all_values.append(value)
 
+    if not all_values:
+        return 0
+
     return sum(all_values) / len(all_values)
+
+
+def average_homework_grade(list_student, name_course):
+    return average_rating(list_student, name_course, 'grades')
 
 
 def average_lecture_rating(list_lecture, name_course):
-    all_values = []
-
-    for lecture in list_lecture:
-        if name_course in lecture.lecture_evaluations:
-            for value in lecture.lecture_evaluations[name_course]:
-                all_values.append(value)
-
-    return sum(all_values) / len(all_values)
+    return average_rating(list_lecture, name_course, 'lecture_evaluations')
